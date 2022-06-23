7e8afd6ba86425b67bd9d563a042e83894c3f142
s50551 <s50551@beuth-hochschule.de>
2022-06-23 20:16:57 +0200

    M2 finished

--- src/components/CreateUserDialog.js
+++ src/components/CreateUserDialog.js
@@ -78,9 +78,9 @@ class CreateUserDialog extends Component {
         let buttonState
         let errorWhileCreating
 
-        buttonState = <Button id="OpenCreateUserDialogButton" variant="warning" onClick={this.handleShow}>
-            <i className="fa-solid fa-square-plus"></i>
-            New User
+        buttonState =   <Button id="OpenCreateUserDialogButton" variant="warning" onClick={this.handleShow}>
+                            <i className="fa-solid fa-square-plus px-1"></i>
+                                New User
                         </Button>
 
         if(error){
