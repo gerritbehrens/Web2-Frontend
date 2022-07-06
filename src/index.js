ca6cfab050ca539c99c4a3135f3b2bae442cec3d
gnb <gerrit.n.behrens@gmail.com>
2022-07-06 22:42:57 +0200

    M3 cleanUp

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import thunk from 'redux-thunk'
 
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './fontAwesome/css/all.css'
@@ -22,8 +21,3 @@ root.render(
     <App />
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
